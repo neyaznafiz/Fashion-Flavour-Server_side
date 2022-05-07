3b465a167019f9faa8dc4247dee0503177004f97
niiyazobug <niiyaz5555@gmail.com>
2022-05-07 11:55:36 +0600

    e

---
+++ index.js
@@ -0,0 +1,118 @@
+const express = require('express')
+require('dotenv').config()
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const ObjectId = require('mongodb').ObjectId
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port = process.env.PORT || 5000
+const app = express()
+
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+// function jwtVerify(req, res, next) {
+//     const authHeader = req.headers.authorization
+//     console.log(authHeader);
+//     if (!authHeader) {
+//         return res.status(401).send({ message: 'unauthorized access' })
+//     }
+
+//     else {
+//         const token = authHeader.split(' ')[1]
+//         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//             if (err) {
+//                 return res.status(403).send({ message: 'Forbidden access done' })
+//             }
+//             console.log('decoded', decoded);
+//             req.decoded = decoded
+//             next()
+//         })
+//     }
+// }
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.FASHIONFLAVOUR_USER}:${process.env.FASHIONFLAVOUR_PASS}@cluster0.mvgy0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+const run = async () => {
+
+    try {
+
+        await client.connect();
+        const productsCollection = client.db('fashionFlavour').collection('dress')
+
+
+        // jwt
+        app.post('/login', async (req, res) => {
+            const user = req.body
+            const accessJwtToken = jwt.sign(user, process.env.ACCESS_JWTTOKEN, {
+                expiresIn: '10d'
+            })
+            res.send({ accessJwtToken })
+        })
+
+        // All product get
+        app.get('/dress', async (req, res) => {
+            const query = {}
+            const result = await productsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        // Get product by id
+        app.get('/dress/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: id };
+            const result = await productsCollection.findOne(query)
+            res.send(result);
+        });
+
+        // Post product
+        app.post('/dress', async (req, res) => {
+            const query = req.body
+            const result = await productsCollection.insertOne(query)
+            res.send(result)
+        })
+
+        // Filter product by email
+        app.get('/mydress', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+            const result = await productsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+
+        // Delete product
+        app.delete('/dress/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: id }
+            const results = await productsCollection.deleteOne(query)
+            console.log(results);
+            res.send(results)
+        })
+
+    }
+    finally { }
+}
+
+run().catch(console.dir)
+
+
+
+app.get('/', (req, res) => {
+    res.send('hey bro !! I am working');
+})
+
+app.listen(port, () => {
+    console.log('Listning to port', port);
+})
